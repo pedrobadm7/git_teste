2ecc99b8e0fb6f8c93bbb8abf4faae904c889121
Pedro Barros <pedrobars7a@gmail.com>
2021-03-12 08:41:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let clicou = (title) => {
+   let documentName = document.getElementById("title");
+   documentName.innerHTML=title
+}
+
+//onmouseover: Algo acontece quando passa o mouse em cima da coisa
+//onmouseout: Algo acontece quando tira o mouse de cima da coisa
